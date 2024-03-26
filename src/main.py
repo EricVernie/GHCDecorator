4b1557c5ab260283c2d60b37957774ae06e864aa
Eric Vernié <eric.vernie@hotmail.fr>
2024-03-26 15:50:35 +0100

    change flask parameters

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ from module.decorator import validate_token
 app = Flask(__name__)
 
 @app.route('/')
+def health():
+    return 'status, healthy!'
+
+@app.route('/hello')
 @validate_token('Data.Reader')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(port=80,debug=True)
+    app.run(host="20.160.177.105",port=80,debug=True)
